759768ec35cd1762f43771dc7eecc425d0d69c89
Shirley Huang <shirley.huang.93@gmail.com>
2019-08-04 18:52:56 -0700

    new scenes

--- clean_data.py
+++ clean_data.py
@@ -1,11 +1,12 @@
 import pandas as pd
 
-df = pd.read_csv("/Users/shirley/Desktop/players.csv")
+df = pd.read_csv("/Users/shirley/shuan102.github.io/players.csv")
 
 df = df.dropna()
 df[["draft_year"]] = df[["draft_year"]].apply(pd.to_numeric)
 df = df[(df.draft_year >= 1947) & (df.draft_year <= 2018)]
 df = df[df["career_FG%"] != '-']
 df = df[df["career_FG3%"] != '-']
+df = df[df["career_FT%"] != '-']
 
-df.to_csv("/Users/shirley/Desktop/players-clean.csv")
+df.to_csv("/Users/shirley/shuan102.github.io/players-clean.csv")
